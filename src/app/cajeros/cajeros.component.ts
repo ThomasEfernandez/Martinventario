e1f6f1253e45c15c72d6e285458eac93910dd323
Agustina Martín <agustinamariamartin@gmail.com>
2024-10-24 16:05:41 -0300

    Agrego funcionalidad al cajero

--- src/app/cajeros/cajeros.component.ts
+++ src/app/cajeros/cajeros.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Cajeros } from '../interfaces/Cajeros.interface';
 import { ProductosComponent } from '../productos/productos.component';
 import { VentasComponent } from '../ventas/ventas.component';
+import { elementAt } from 'rxjs';
 
 @Component({
   selector: 'app-cajeros',
@@ -14,6 +15,84 @@ import { VentasComponent } from '../ventas/ventas.component';
 })
 export class CajerosComponent {
   listaCajeros: Set<Cajeros> = new Set<Cajeros>() ;
+
+  agregarCajero ( nuevoCajero:Cajeros ) {
+    this.listaCajeros.add(nuevoCajero) ;
+  }
+
+  eliminarCajeroPorId (id:number) {
+    let eliminarCajero: Cajeros | undefined ; //para que tenga valor por defecto
+
+    this.listaCajeros.forEach(element => {
+      if ( element.id === id ) {
+        element.estado = false ;
+        eliminarCajero = element ;
+      }
+    }) ;
+
+    if ( eliminarCajero ) {
+      this.listaCajeros.delete(eliminarCajero) ;
+      console.log(`Cajero con id ${{id}} fue eliminado.`) ; //prueba
+    } else {
+      console.log(`Cajero con id ${{id}} no encontrado.`) ; //prueba
+    }
+  }
+
+  eliminarCajeroPorUsuario (usuario: string) {
+    let eliminarCajero: Cajeros | undefined ;
+
+    this.listaCajeros.forEach(element => {
+      if ( element.usuario === usuario ) {
+        element.estado = false ;
+        eliminarCajero = element ;
+      }
+    }) ;
+
+    if ( eliminarCajero ) {
+      this.listaCajeros.delete(eliminarCajero) ;
+      console.log(`Cajero con nombre: ${{usuario}} fue eliminado.`) ; //prueba
+    } else {
+      console.log(`Cajero con nombre: ${{usuario}} no encontrado.`) ; //prueba
+    }
+  }
+
+  buscarCajeroPorId (id:number) {
+    let encontrado: Cajeros | undefined ;
+
+    this.listaCajeros.forEach((element)=>{
+      if ( element.id === id ) {
+        encontrado = element ;
+      }
+    })
+
+    //codigo de prueba
+    if ( encontrado ) {
+      console.log("Cajero encontrado: ", encontrado) ;
+    } else {
+      console.log(`Cajero con id ${{id}} no encontrado`) ;
+    }
+
+    return encontrado ;
+  }
+
+  buscarCajeroPorUsuario (usuario:string) {
+    let encontrado: Cajeros | undefined ;
+
+    this.listaCajeros.forEach((element)=>{
+      if ( element.usuario === usuario ) {
+        encontrado = element ;
+      }
+    })
+
+    //codigo de prueba
+    if ( encontrado ) {
+      console.log("Cajero encontrado: ", encontrado) ;
+    } else {
+      console.log(`Cajero con usuario: ${{usuario}} no encontrado`) ;
+    }
+
+    return encontrado ;
+  }
 
   //registrar venta
 
