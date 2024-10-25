33a58fc72638cb1ad5b9a9768bbda1a14f0ab66f
Agustin Rojas <agustinrojas259@gmail.com>
2024-10-25 19:05:54 -0300

    Inicio de sesión
    Componente sesion que verifica si el usuario existe al iniciar sesion y si la contraseña es correcta, cambio en el navbar, introduccion del logo y nombre, algunos cambios menores, estructura de carpetas

--- src/app/usuario/services/usuario.service.ts
+++ src/app/usuario/services/usuario.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { Usuario } from '../interfaces/Usuario.interface';
+import { Usuario } from '../interfaces/usuario.interface';
 
 @Injectable({
   providedIn: 'root'
