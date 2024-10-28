c43496c343c99c529ffb6816ea8dfe4bf7f86539
Agustina Martín <agustinamariamartin@gmail.com>
2024-10-28 10:39:27 -0300

    Servicios etiqueta y categoria

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,29 +1,19 @@
 import { Routes } from '@angular/router';
 import { IniciarSesionComponent } from './pagina-inicio-sesion/components/iniciar-sesion/iniciar-sesion.component';
 import { AgregarProductoComponent } from './producto/components/agregar-producto/agregar-producto.component';
-import { ProveedoresComponent } from './proveedores/proveedores.component';
-import { CategoriasComponent } from './categorias/categorias.component';
 import { ModificarProductoComponent } from './producto/components/modificar-producto/modificar-producto.component';
 import { EliminarProductoComponent } from './producto/components/eliminar-producto/eliminar-producto.component';
 
 export const routes: Routes = [
-  
+
   {
     path:'',
     component: IniciarSesionComponent
   },
   // {
-  //   path:'', 
+  //   path:'',
   //   component: PaginaPrincipalComponent
   // },
-  {
-    path:'proveedores',
-    component:ProveedoresComponent
-  },
-  {
-    path:'categorias',
-    component:CategoriasComponent
-  },
   {
     path:'agregarProducto',
     component:AgregarProductoComponent
