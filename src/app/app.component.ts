214abf86f3ba6344a8e5e354d3493fb931f8747d
Agustin Rojas <agustinrojas259@gmail.com>
2024-10-30 01:11:36 -0300

    reescritura de agregar etiqueta

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,24 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { IniciarSesionComponent } from './pagina-inicio-sesion/components/iniciar-sesion/iniciar-sesion.component';
-import { RealizarPedidoComponent } from "./compra/components/realizar-pedido/realizar-pedido.component";
-import { PaginaPedidoComponent } from "./compra/pages/pagina-pedido/pagina-pedido.component";
-import { PaginaPrincipalComponent } from "./pagina-principal/pagina-principal.component";
-import { ListarCategoriasComponent } from "./categoria/components/listar-categorias/listar-categorias.component";
-import { ListarProductosComponent } from "./producto/components/listar-productos/listar-productos.component";
+import { RealizarPedidoComponent } from './compra/components/realizar-pedido/realizar-pedido.component';
+import { PaginaPedidoComponent } from './compra/pages/pagina-pedido/pagina-pedido.component';
+import { PaginaPrincipalComponent } from './pagina-principal/pagina-principal.component';
+import { ListarCategoriasComponent } from './categoria/components/listar-categorias/listar-categorias.component';
+import { ListarProductosComponent } from './producto/components/listar-productos/listar-productos.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, IniciarSesionComponent, RealizarPedidoComponent, PaginaPedidoComponent, PaginaPrincipalComponent, ListarCategoriasComponent, ListarProductosComponent],
+  imports: [
+    RouterOutlet,
+    IniciarSesionComponent,
+    RealizarPedidoComponent,
+    PaginaPedidoComponent,
+    PaginaPrincipalComponent,
+    ListarCategoriasComponent,
+    ListarProductosComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
