f865db02806bafb1c8247416a8fb5caca706fe31
Agustina Martín <agustinamariamartin@gmail.com>
2024-10-31 10:37:06 -0300

    Service etiquetas y metodo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { IniciarSesionComponent } from './pagina-inicio-sesion/components/iniciar-sesion/iniciar-sesion.component';
 import { AgregarProductoComponent } from './producto/components/agregar-producto/agregar-producto.component';
 import { PaginaPedidoComponent } from './compra/pages/pagina-pedido/pagina-pedido.component';
+import { ListarCategoriasComponent } from './categoria/components/listar-categorias/listar-categorias.component';
 
 export const routes: Routes = [
   {
@@ -15,13 +16,18 @@ export const routes: Routes = [
   {
     path: 'agregarProducto',
     component: AgregarProductoComponent,
-  },{
+  },
+  {
     path: 'pedido',
     component: PaginaPedidoComponent,
+  },
+  {
+    path: 'listarCategorias',
+    component: ListarCategoriasComponent,
   },
   {
     path: '**',
     redirectTo: '',
     pathMatch: 'full',
-  }
+  },
 ];
