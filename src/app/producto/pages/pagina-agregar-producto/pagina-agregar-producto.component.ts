fd954b8c2332da63e427b6b358e56193b3cd4eba
Thomas_Fernandez <thomyezequiel@hotmail.com>
2024-11-01 08:16:52 -0300

    algunos ruteos, cambio de navbar en productos

--- src/app/producto/pages/pagina-agregar-producto/pagina-agregar-producto.component.ts
+++ src/app/producto/pages/pagina-agregar-producto/pagina-agregar-producto.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { AgregarProductoComponent } from "../../components/agregar-producto/agregar-producto.component";
 import { NavbarVistaAdminComponent } from "../../../nav/components/navbar-admin/navbar-vista-admin/navbar-vista-admin.component";
+import { ListarCategoriasComponent } from "../../../categoria/components/listar-categorias/listar-categorias.component";
+import { ListarProductosComponent } from "../../components/listar-productos/listar-productos.component";
+import { NavbarVistaProductosAdminComponent } from "../../../nav/components/navbar-admin/navbar-vista-productos-admin/navbar-vista-productos-admin.component";
 
 @Component({
   selector: 'app-pagina-agregar-producto',
   standalone: true,
-  imports: [ AgregarProductoComponent, NavbarVistaAdminComponent,NavbarVistaAdminComponent],
+  imports: [AgregarProductoComponent, NavbarVistaAdminComponent, NavbarVistaAdminComponent, ListarCategoriasComponent, ListarProductosComponent, NavbarVistaProductosAdminComponent],
   templateUrl: './pagina-agregar-producto.component.html',
   styleUrl: './pagina-agregar-producto.component.css'
 })
