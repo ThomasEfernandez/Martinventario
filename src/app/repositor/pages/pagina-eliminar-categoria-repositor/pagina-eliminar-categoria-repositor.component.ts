d10c10c0762d209c4157c0739a735efbb610998e
Agustina Martín <agustinamariamartin@gmail.com>
2024-11-11 11:51:25 -0300

    Rutas y componentes eliminar

---
+++ src/app/repositor/pages/pagina-eliminar-categoria-repositor/pagina-eliminar-categoria-repositor.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pagina-eliminar-categoria-repositor',
+  standalone: true,
+  imports: [],
+  templateUrl: './pagina-eliminar-categoria-repositor.component.html',
+  styleUrl: './pagina-eliminar-categoria-repositor.component.css'
+})
+export class PaginaEliminarCategoriaRepositorComponent {
+
+}
---
+++ src/app/repositor/pages/pagina-eliminar-etiqueta-repositor/pagina-eliminar-etiqueta-repositor.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pagina-eliminar-etiqueta-repositor',
+  standalone: true,
+  imports: [],
+  templateUrl: './pagina-eliminar-etiqueta-repositor.component.html',
+  styleUrl: './pagina-eliminar-etiqueta-repositor.component.css'
+})
+export class PaginaEliminarEtiquetaRepositorComponent {
+
+}
