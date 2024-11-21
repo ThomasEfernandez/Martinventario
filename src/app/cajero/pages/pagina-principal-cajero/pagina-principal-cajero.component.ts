03a855aff1fa10b3940b8956d955cdb841773f5f
Agustin Rojas <agustinrojas259@gmail.com>
2024-11-20 21:52:13 -0300

    fondos para la pagina principal del repositor y cajero

--- src/app/cajero/pages/pagina-principal-cajero/pagina-principal-cajero.component.ts
+++ src/app/cajero/pages/pagina-principal-cajero/pagina-principal-cajero.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { FondoCajeroComponent } from 'app/fondo/components/fondo-cajero/fondo-cajero.component';
 import { NavbarCajeroComponent } from 'app/nav/components/navbar-cajero/navbar-cajero.component';
 
 @Component({
   selector: 'app-pagina-principal-cajero',
   standalone: true,
-  imports: [NavbarCajeroComponent],
+  imports: [NavbarCajeroComponent, FondoCajeroComponent],
   templateUrl: './pagina-principal-cajero.component.html',
   styleUrl: './pagina-principal-cajero.component.css',
 })
