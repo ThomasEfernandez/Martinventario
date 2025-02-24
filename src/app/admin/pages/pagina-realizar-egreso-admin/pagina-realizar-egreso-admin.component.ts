dc3b9caa3e00d68646f42496208d8c3e9a405008
Agustina Martín <agustinamariamartin@gmail.com>
2025-02-24 19:47:35 -0300

    Cambio en salidas (cajero)

--- src/app/admin/pages/pagina-realizar-egreso-admin/pagina-realizar-egreso-admin.component.ts
+++ src/app/admin/pages/pagina-realizar-egreso-admin/pagina-realizar-egreso-admin.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { NavbarAdminComponent } from '../../../nav/components/navbar-admin/navbar-admin.component';
-import { RealizarEgresoAdminComponent } from 'app/venta/components/realizar-venta-admin/realizar-venta-admin.component';
+import { RealizarEgresoAdminComponent } from 'app/venta/components/realizar-egreso-admin/realizar-egreso-admin.component';
 
 @Component({
   selector: 'app-pagina-realizar-egreso-admin',
