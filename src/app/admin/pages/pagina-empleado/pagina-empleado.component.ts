de853618ad44213e32e9e4cd103ca615276c57d0
Agustin Rojas <agustinrojas259@gmail.com>
2025-03-25 20:28:46 -0300

    .

--- src/app/admin/pages/pagina-empleado/pagina-empleado.component.ts
+++ src/app/admin/pages/pagina-empleado/pagina-empleado.component.ts
@@ -1,15 +1,12 @@
 import { Component } from '@angular/core';
-import { NavbarAdminComponent } from "../../../nav/components/navbar-admin/navbar-admin.component";
-import { EmpleadosComponent } from "../../empleados/empleados/empleados.component";
+import { NavbarAdminComponent } from '../../../nav/components/navbar-admin/navbar-admin.component';
+import { EmpleadosComponent } from '../../empleados/empleados/empleados.component';
 
 @Component({
   selector: 'app-pagina-empleado',
   standalone: true,
   imports: [NavbarAdminComponent, EmpleadosComponent],
   templateUrl: './pagina-empleado.component.html',
-  styleUrl: './pagina-empleado.component.css'
+  styleUrl: './pagina-empleado.component.css',
 })
-export class PaginaEmpleadoComponent {
-
-}
-
+export class PaginaEmpleadoComponent {}
