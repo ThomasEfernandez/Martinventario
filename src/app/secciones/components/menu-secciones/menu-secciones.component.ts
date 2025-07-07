20da42b3f931f5d1a442231df7a11042b8e6df94
Agustin Rojas <agustinrojas259@gmail.com>
2025-07-07 18:31:27 -0300

    Modularizacion de estadisticas

--- src/app/secciones/components/menu-secciones/menu-secciones.component.ts
+++ src/app/secciones/components/menu-secciones/menu-secciones.component.ts
@@ -1,18 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { EstadisticasEmpleadosComponent } from "../../../estadistica/VER/estadisticas-empleados/estadisticas-empleados.component";
-import { ListarIngresosComponent } from "../../../ingreso/components/listar-ingresos/listar-ingresos.component";
-import { ListarEgresosComponent } from "../../../egreso/components/listar-egresos/listar-egresos.component";
-import { ListarPedidosComponent } from "../../../pedido/components/listar-pedidos/listar-pedidos.component";
-import { EstadisticasIngresosComponent } from "../../../estadistica/VER/estadisticas-ingresos/estadisticas-ingresos.component";
-import { EstadisticasEgresosComponent } from "../../../estadistica/VER/estadisticas-egresos/estadisticas-egresos.component";
-import { EstadisticasPedidosComponent } from "../../../estadistica/VER/estadisticas-pedidos/estadisticas-pedidos.component";
 import { Usuario } from 'app/usuario/interfaces/usuario.interface';
 
 @Component({
   selector: 'app-menu-secciones',
   standalone: true,
-  imports: [RouterModule, EstadisticasEmpleadosComponent, EstadisticasIngresosComponent, EstadisticasEgresosComponent, EstadisticasPedidosComponent],
+  imports: [RouterModule],
   templateUrl: './menu-secciones.component.html',
   styleUrl: './menu-secciones.component.css',
 })
@@ -24,6 +17,6 @@ export class MenuSeccionesComponent {
     usuario: '',
     contrasena: '',
     tipo: '',
-    estado: false
+    estado: false,
   };
 }
