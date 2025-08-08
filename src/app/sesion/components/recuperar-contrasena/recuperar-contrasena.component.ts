d8c6760789d42010041fc4066fda434b856d771d
Agustin Rojas <agustinrojas259@gmail.com>
2025-08-07 21:07:45 -0300

    API contrasena test

--- src/app/sesion/components/recuperar-contrasena/recuperar-contrasena.component.ts
+++ src/app/sesion/components/recuperar-contrasena/recuperar-contrasena.component.ts
@@ -3,6 +3,7 @@ import { ReactiveFormsModule, FormBuilder, Validators } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { Usuario } from 'app/usuario/interfaces/usuario.interface';
 import { UsuarioService } from 'app/usuario/services/usuario.service';
+import emailjs from '@emailjs/browser';
 
 @Component({
   selector: 'app-recuperar-contrasena',
@@ -26,22 +27,49 @@ export class RecuperarContrasenaComponent {
     mail: [''],
   });
 
+  // recuperarContrasena() {
+  //   if (this.formulario.invalid) return;
+  //   const sesion = this.formulario.getRawValue();
+  //   const user = this.buscarUsuario(sesion);
+
+  //   if (user) {
+  //     console.log(user.mail);
+  //     user.contrasena
+  //     /**
+  //      * ACA IRIA LO DE NOGAR
+  //      *
+  //      * LA FORMA DE CONSEGUIR EL MAIL DEL USUARIO QUE ES INGRESADO EN EL INPUT ES CON:
+  //      * user.mail
+  //      * ESTO TE DEVUELVE EL MAIL EN FORMATO STRING DEL OBJETO user DE TIPO Usuario
+  //      *
+  //      * ESTA FUNCION SE EJECUTA CUANDO SE PRESIONA EL BOTON RECUPERAR CONTRASEÑA
+  //      */
+  //   }
+  // }
+
   recuperarContrasena() {
     if (this.formulario.invalid) return;
     const sesion = this.formulario.getRawValue();
     const user = this.buscarUsuario(sesion);
 
     if (user) {
-      console.log(user.mail);
-      /**
-       * ACA IRIA LO DE NOGAR
-       * 
-       * LA FORMA DE CONSEGUIR EL MAIL DEL USUARIO QUE ES INGRESADO EN EL INPUT ES CON:
-       * user.mail
-       * ESTO TE DEVUELVE EL MAIL EN FORMATO STRING DEL OBJETO user DE TIPO Usuario
-       * 
-       * ESTA FUNCION SE EJECUTA CUANDO SE PRESIONA EL BOTON RECUPERAR CONTRASEÑA
-       */
+      console.log('Enviando contraseña a:', user.mail);
+
+      emailjs
+        .send(
+          'gmailnogar',
+          'template_j2lm5hg',
+          {
+            to_email: user.mail,
+            contrasena: user.contrasena,
+          },
+          'PgrLA8Z8f560V_agx'
+        )
+        .catch((error) => {
+          console.error('Error al enviar el correo:', error);
+        });
+    } else {
+      console.log('Usuario no encontrado');
     }
   }
 
